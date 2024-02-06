95ef58e15eaf94928982973c481d3b14fa71fea4
Matias Cortese <matiasemanuelcortese@gmail.com>
2024-02-05 21:18:38 -0300

    proyecto ya funcionando

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 var readlineSync = require('readline-sync');
-// TO-DO
-// Considerar modulo para parsear el input
-// valor inicial de la caja
+
 let dinero = 1000;
+let operacionARealizar;
+let montoAOperar;
 const listaUltimosMovimientos = [];
-// Obtener movimiento
-// let movimiento = process.argv[2];
-// // Obtener valor parsear con parseInt();
-// let valorDinero2 = process.argv[3];
-// console.log("datos de la operacion", + " ", valorDinero2)
-// Parsear el numero que ingresa x consola toInt()
-// hacer un switch con cada caso
 
 function menu(){
     console.log("Bienvenido a la caja. Indicanos qué operación deseas realizar: extraer, depositar, consultar saldo, ver últimos movimientos o salir");
-    const operacionARealizar = readlineSync.question("Qué operación deseas realizar? ");
-    const montoAOperar = readlineSync.question("Qué monto deseas operar?");
-    const montoParseado = parseInt(montoAOperar);
-    console.log("Vamos a hacer una " + operacionARealizar + " y vamos a operar con $ " + montoParseado);
+    operacionARealizar = readlineSync.question("Qué operación deseas realizar? ");
+    if (operacionARealizar === "extraer" || operacionARealizar === "depositar") {
+        montoAOperar = readlineSync.question("Qué monto deseas operar? ");
+        montoParseado = parseInt(montoAOperar);
+        console.log("Vamos a hacer una " + operacionARealizar + " y vamos a operar con $ " + montoParseado);
+    };
+    switch (operacionARealizar) {
+        case "extraer":
+            extraer(montoAOperar)
+            break;
+        case "depositar":
+            depositar(montoAOperar)
+            break
+        case "consultar saldo":
+            consultaSaldo(dinero)
+            break
+        case "ultimos movimientos":
+            ultimosMovimientos()
+            break;
+        case "salir":
+            salir()
+        default:
+            console.log("No es una opcion válida");
+            break;
+    }
 }
 
 function extraer(montoAOperar){
@@ -27,20 +41,23 @@ function extraer(montoAOperar){
         const infoDelMovimiento = "Extraído: $ " + montoAOperar;
         listaUltimosMovimientos.push(infoDelMovimiento);
         console.log(mensaje)
+        menu();
         return dinero;
     }
     else {
         console.log("No hay fondos");
+        menu();
     }
 }
 
 function depositar(montoAOperar){
-    dinero = dinero + montoAOperar;
+    dinero = dinero + parseInt(montoAOperar);
     console.log("Tu saldo disponible ahora es de: " + dinero);
     const mensaje = "Depositamos: $" + montoAOperar + " y el saldo disponible luego del depósito es de $" + dinero;
     const infoDelMovimiento = "Depositado: $" + montoAOperar;
     listaUltimosMovimientos.push(infoDelMovimiento);
     console.log(mensaje);
+    menu();
     return dinero;
 }
 
@@ -48,24 +65,28 @@ function consultaSaldo(){
     console.log("Tu saldo disponible es de: ", + dinero);
     const infoDelMovimiento = "Consultado saldo: $" + dinero;
     listaUltimosMovimientos.push(infoDelMovimiento);
+    menu();
     return dinero;
 }
 
 function ultimosMovimientos(){
-    for(let i = 0; i <= listaUltimosMovimientos.length; i++) {
-        console.log(listaUltimosMovimientos[i]);
+    if (listaUltimosMovimientos.length == 0) {
+        console.log("Aun no hay movimientos realizados en esta caja")
+    } else {
+        for(let i = 0; i <= listaUltimosMovimientos.length; i++) {
+            console.log(listaUltimosMovimientos[i]);
+        }
     }
+    menu();
 }
 
 function salir(){
-
+    console.log("salimos");
+    return;
 }
 
 function main(){
-    extraer(200);
-    depositar(300);
-    consultaSaldo();
-    ultimosMovimientos();
+    menu();
 }
 
 main();
--- migue.js
+++ migue.js
@@ -1,35 +1,72 @@
+// argumento1 para ingresar: extraer, depositar, ultimos movimientos, saldo,salir
+const argumento1 = process.argv[2];
+// argumento2 para ingresar monto de deposito o extraccion
+const argumento2 = process.argv[3];
+let opcion = argumento1;
+let dinero = parseFloat(argumento2);
+
 let dineroEnCuenta = 1000;
 let movimientos = [];
 
-function depositoExtraccion(opcion, dinero) {
-    if (opcion === "depositar") {
-        if (!isNaN(dinero) && dinero > 0) {
-            dineroEnCuenta += dinero;
-            console.log("Has depositado: " + dinero + ". Nuevo saldo: " + dineroEnCuenta);
-        } else {
-            console.log("Ingresa un monto válido.");
-        }
-    } else if (opcion === "extraer") {
+function depositar(dinero) {
+    if (!isNaN(dinero) && dinero > 0) {
+        dineroEnCuenta += dinero;
+        console.log("Has depositado: $" + dinero + ". Nuevo saldo: $" + dineroEnCuenta);
+        movimientos.push("deposito: $"+ dinero)
+    } else {
+        console.log("Ingresa un monto válido.");
+    }
+    
+} 
+
+function extraer(dinero) {
         if (!isNaN(dinero) && dinero > 0 && dinero <= dineroEnCuenta) {
             dineroEnCuenta -= dinero;
-            console.log("Has extraído: " + dinero + ". Nuevo saldo: " + dineroEnCuenta);
+            console.log("Has extraído: $" + dinero + ". Nuevo saldo: $" + dineroEnCuenta);
+            movimientos.push("extracción: $"+ dinero)
         } else if (dinero > dineroEnCuenta) {
             console.log("Fondos insuficientes.");
         } else {
             console.log("Ingresa un monto válido.");
         }
-    } else {
-        console.log("Opción no válida. Debe ser 'depositar' o 'extraer'.");
-    }
+}
+
+function saldo(){
+    console.log("Tu saldo actual es de: $"+ dineroEnCuenta);
+    movimientos.push("Consulta de saldo: $"+dineroEnCuenta)
+}
+
+function ultimosMovimientos(){
+    console.log("Ultimos Movimientos");
+    console.log(movimientos)
+}
+
+function salir(){
+    console.log("Salimos del programa");
+    return;
 }
 
 function main() {
-    const argumento1 = process.argv[2];
-    const argumento2 = process.argv[3];
-    let opcion = argumento1;
-    let dinero = parseFloat(argumento2);
-
-    depositoExtraccion(opcion, dinero);
+    console.log("Bienvenido al ATM II - Para realizar una acción indique:")
+    switch (opcion) {
+        case "depositar":
+            depositar(dinero);
+            break;
+        case "extraer":
+            extraer(dinero);
+            break;
+        case "saldo":
+            saldo();
+            break;
+        case "movimientos":
+            ultimosMovimientos();
+            break;
+        case "salir":
+            salir();
+            break;
+        default:
+            console.log("No es una opcion válida");
+        }
 }
 
 main();
